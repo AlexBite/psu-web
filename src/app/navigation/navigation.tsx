bbedce98d0e28909af556cb1e226cd23bb968b79
Alexander Bologov <sano.strike14110@gmail.com>
2023-03-02 04:26:08 +0500

    Подключил бутстрап

--- src/app/navigation/navigation.tsx
+++ src/app/navigation/navigation.tsx
@@ -1,25 +1,38 @@
 import styles from './navigation.module.scss';
 import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 /* eslint-disable-next-line */
-export interface NavigationProps {}
+export interface NavigationProps {
+}
+
+const ROUTES = [
+	{
+		path: '/',
+		title: 'Главная'
+	},
+	{
+		path: '/contacts',
+		title: 'Контакты'
+	},
+	{
+		path: '/teachers',
+		title: 'Преподаватели'
+	}
+]
 
 export function Navigation(props: NavigationProps) {
-  return (
-      <nav>
-          <ul>
-              <li>
-                  <Link to="/">Главная</Link>
-              </li>
-              <li>
-                  <Link to="/contacts">Контакты</Link>
-              </li>
-              <li>
-                  <Link to="/teachers">Преподаватели</Link>
-              </li>
-          </ul>
-      </nav>
-  );
+	const navLinks = ROUTES.map((r) => <Nav.Link as={Link} to={r.path}>{r.title}</Nav.Link>)
+	return (
+		<Navbar className={styles['container']} expand="lg">
+			<Container>
+				<Navbar.Brand as={Link} to="/">ПГНИУ</Navbar.Brand>
+				<Nav>
+					{navLinks}
+				</Nav>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default Navigation;
