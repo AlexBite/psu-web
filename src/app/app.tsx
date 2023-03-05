56d787c7036c8c823fa1adfd89013443f6b5e3a6
Alexander Bologov <sano.strike14110@gmail.com>
2023-03-05 16:54:48 +0500

    router fix

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './app.module.scss';
 import { Layout } from './layout/layout';
 import Main from './main/main';
@@ -9,16 +9,14 @@ import Teachers from './teachers/teachers';
 import NotFound from './not-found/not-found';
 
 export const App = () => (
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Main />}></Route>
-        <Route path="contacts" element={<Contacts />}></Route>
-        <Route path="teachers" element={<Teachers />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Route>
-    </Routes>
-  </HashRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Main />}></Route>
+      <Route path="contacts" element={<Contacts />}></Route>
+      <Route path="teachers" element={<Teachers />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Route>
+  </Routes>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
